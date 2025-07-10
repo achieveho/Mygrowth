75b3c350aa1f8643533494684c4c4d8eb85ccf35
achieveho <achieveho718@gmail.com>
2025-07-10 10:38:01 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    result = 0
+    if num_list[-1] > num_list[-2]:
+        result = num_list[-1] - num_list[-2]
+        num_list.append(result)
+    else:
+        result = num_list[-1] * 2
+        num_list.append(result)
+        
+    return num_list

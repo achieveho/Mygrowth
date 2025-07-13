0c7b52161462cb24d1369ce06338d5706e6c12a3
SeongHoCHOI <achieveho718@gmail.com>
2025-07-14 00:11:47 +0900

    Saved other people's well-written code for my growth.

--- 프로그래머스/0/181834. l로 만들기/make_l.py
+++ 프로그래머스/0/181834. l로 만들기/make_l.py
@@ -1,2 +1,7 @@
 def soultion01(myString):
-    
+    return myString.translkate(str.maketrans('abcdefghijk', 'lllllllllll'))
+
+
+def solution02(myString):
+    answer = [x if x > 'l' else 'l' for x in myString]
+    return ''.join(answer)

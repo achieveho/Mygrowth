097c3429b7ac48d9530212e7a963d9f570d47302
achieveho <achieveho718@gmail.com>
2025-07-16 22:45:27 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.84 ms, Memory: 9.92 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                answer[i][j] = 1
+            else:
+                answer[i][j] = 0
+    
+    return answer

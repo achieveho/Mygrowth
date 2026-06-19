3adf4df7d448cee337421d0dd1605154ebe04c70
ACHIEVEHO <achieveho718@gmail.com>
2026-06-19 17:46:45 +0900

    1. SQLite3를 사용하여 'SQLBolt.db'를 생성하였습니다. 2. VSCode에서 .sql 파일을 생성하여 SQLBolt에서 사용한 Movies 테이블을 이름만 'L002_Movies'로 변경하여 생성하였습니다. 3. 'Exercise'에서 작업한 5개의 쿼리를 생성하는 구문을 VSCode에서 .sql 파일을 생성해 작성하였습니다. 4. DBeaver에서 'SQLBolt.db' 파일을 열어서 GUI로 내부 구조를 확인하였고, SQL Text Editor로 Excercise에서 작업한 구문을 한 번 더 입력해봤습니다.

---
+++ 005_SQLBolt/002_SELECT_Queries_101/Exercise.sql
@@ -0,0 +1,14 @@
+-- 1. Find the title of each film.
+SELECT Title From L002_Movies;
+
+-- 2. Find the director of each film.
+SELECT Director FROM L002_Movies;
+
+-- 3. Find the title and director of each film.
+SELECT Title, director FROM L002_Movies;
+
+-- 4. Find the title and year of each film.
+SELECT Title, Year FROM L002_Movies;
+
+-- 5. Find all the information about each film.
+SELECT * FROM L002_Movies;
